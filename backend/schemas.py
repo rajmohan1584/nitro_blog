cff73b1417642d1c12e4e0b7fa528555df200ff9
Raj Mohan <rajmohan@hotmail.com>
2026-09-21 19:18:37 -0400

    Save point before pydantic integration

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,6 +3,18 @@
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50, description="The username of the user")
+    email: EmailStr = Field(max_length=120,description="The email of the user")
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int = Field(..., description="The ID of the user")
+    image_file: str | None = Field(default=None, description="The image file of the user")
+    image_path: str = Field(..., description="The image path of the user")
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100, description="The title of the post")
@@ -16,10 +28,12 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    pass
+    user_id: int = Field(..., description="The ID of the user (TODO: Get from Session)")
 
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int = Field(..., description="The ID of the post")
-    date_posted: str = Field(..., description="The date the post was posted")
+    user_id: int = Field(..., description="The ID of the user (TODO: Same as author.id)")
+    date_posted: datetime = Field(..., description="The date the post was posted")
+    author: UserResponse = Field(..., description="The author of the post")
